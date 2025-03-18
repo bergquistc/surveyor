4d0e7e44528d3113b32f73fde0dffdc270719893
Connor Bergquist <connor.bergquist@bintel.io>
2025-03-18 09:23:59 -0700

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+		config.module.rules.push({
+			test: /\.(xlsx|xls|csv)(\?.*)?$/,
+			use: "webpack-xlsx-loader"
+		})
 
-export default nextConfig;
+		config.module.rules.push({
+			test: /\.(eot|svg|ttf|woff|woff2)$/,
+			loader: "file-loader"
+		})
+
+		// Important: return the modified config
+		return config
+	}
+}
+
+export default nextConfig
